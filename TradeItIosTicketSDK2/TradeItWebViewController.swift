0f4760e7179c06e7b25a4d15a631f2828adfd7bb
Sergey Reznichenko <mutexlock64@gmail.com>
2020-06-24 18:35:16 +0300

    replace deprecated UIWebView with WKWebView

--- TradeItIosTicketSDK2/TradeItWebViewController.swift
+++ TradeItIosTicketSDK2/TradeItWebViewController.swift
@@ -1,24 +1,36 @@
 import UIKit
+import WebKit
 
-class TradeItWebViewController: CloseableViewController, UIWebViewDelegate {
-    @IBOutlet weak var webView: UIWebView!
+
+class TradeItWebViewController: CloseableViewController, WKNavigationDelegate {
+    var webView: WKWebView!
     var url = ""
     var pageTitle = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.webView = WKWebView()
+        self.webView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(webView)
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor)
+        ])
+        self.webView.navigationDelegate = self
         self.navigationItem.title = "Loading...";
         guard let urlObject = URL (string: self.url) else {
             print("TradeIt SDK ERROR: Invalid url provided: " + self.url)
             _ = self.navigationController?.popViewController(animated: true)
             return
         }
-        self.webView.loadRequest(URLRequest(url: urlObject))
+        self.webView.load(URLRequest(url: urlObject))
     }
 
-    // MARK: UIWebViewDelegate
-
-    func webViewDidFinishLoad(_ webView: UIWebView) {
-        self.navigationItem.title = self.pageTitle;
+    // MARK: WKWebViewDelegate
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.navigationItem.title = self.pageTitle
     }
 }
